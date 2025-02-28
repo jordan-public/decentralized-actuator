db94f95bfeb95e153ba97d98c7642ca5255eab7e
jordan <jordan@bitola.com>
2025-02-28 06:18:01 -0700

    Deploy script for Anvil

--- deployAnvil.sh
+++ deployAnvil.sh
@@ -8,9 +8,9 @@ set -e
 . ./.env
 
 # To deploy and verify our contract
-forge script script/DeployAnywhere.s.sol:Deploy --rpc-url "http://127.0.0.1:8545/" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -v
+forge script script/DeployAnywhere.s.sol:Deploy --rpc-url "http://127.0.0.1:8545/" --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast -v
 
-./push_artifacts.sh "DeployAnywhere.s.sol/11155111"
+./push_artifacts.sh "DeployAnywhere.s.sol/31337"
 
 # cd web
 # npm run build
